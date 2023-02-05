cb2b9a45f5956481e48f8b7cc857a0c65943732f
Alexis D <dutaud.alex@gmail.com>
2023-02-05 04:27:22 +0100

    build: header ok

--- frontend/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-function App(): JSX.Element {
-  return <p>app</p>;
-}
-
-export default App;
---
+++ frontend/src/components/App/App.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Header from '../Header/Header';
+
+function App(): JSX.Element {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
+
+// dummy data
+// {
+//   "id": 1,
+//   "business_name": "www.google.com",
+//   "contact_email": "google@gmail.com",
+//   "contact_phone": "0606060606",
+//   "contact_linkedin": "https://www.linkedin.com/in/lindsey-louv/",
+//   "category": "AucuneIdée",
+//   "post": {
+//     "id": 1,
+//     "name": "Maitre du monde"
+//   },
+//   "source": {
+//     "id": 1,
+//     "name": "LinkedId",
+//     "link": "https://www.welcometothejungle.com/fr/companies/enedis/jobs/ingenieure-ingenieur-reseaux-electriques-h-f_bourg-en-bresse_ENEDI_NbpVLVW?q=9026e7f962fd151d177ad5a08c61f3fe&o=1606485"
+//   }
+// }
